dc61dd9fcfd8d31c39895562c76a2d9e430450e3
Divya Paliwal <div.pali@gmail.com>
2024-09-12 23:04:12 +0530

    first commit

---
+++ src/test/java/com/example/vault_spring_boot_demo/VaultSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vault_spring_boot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
